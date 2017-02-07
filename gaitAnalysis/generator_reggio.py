7d8957cd39601e3b99ff26ed6d5c04a537d6aa1b
berga2040 <lucabergamini24@gmail.com>
2017-02-07 19:56:17 +0100

    sistemati errori giganteschi nel codice, prendevamo frame sbagliati e non ordinavamo in lettura, comunque keras non sale oltre il 40%, mentre la mia overfitta il train al 98% in 30 epoche ma fa il 33% sul test

--- gaitAnalysis/generator_reggio.py
+++ gaitAnalysis/generator_reggio.py
@@ -8,7 +8,8 @@ import os
 
 def generator_reggio(folder_path):
     #leggo numero gait data
-    file_names = [folder_path+f for f in os.listdir(folder_path) if f[-7:] == "c3d.npy" ]
+    file_names = [f for f in os.listdir(folder_path) if "c3d" in f ]
+    file_names.sort(lambda x,y : 1 if int(x[:-8]) > int(y[:-8]) else -1)
     total_size = len(file_names)
     #leggo labels
     res_label = numpy.load(folder_path+"labels.npy")
@@ -19,10 +20,10 @@ def generator_reggio(folder_path):
 
     yield [total_size]
     #devo capire quanti elementi allocare
-    file_shape = numpy.load(file_names[0]).shape
+    file_shape = numpy.load(folder_path+file_names[0]).shape
     res_data = numpy.zeros((total_size,file_shape[0],file_shape[1],3))
     for i in xrange(total_size):
-        res_data[i] = numpy.load(file_names[i])
+        res_data[i] = numpy.load(folder_path+file_names[i])
 
     yield [res_data,res_label]
 
